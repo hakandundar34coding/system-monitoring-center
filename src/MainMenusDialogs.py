afe56bee61a1ecdc314deb73409044171a29bef9
Hakan Dündar <hakandundar34coding@gmail.com>
2021-11-21 08:53:21 +0300

    Fixed a bug for running terminal application (Main menu)

--- src/MainMenusDialogs.py
+++ src/MainMenusDialogs.py
@@ -14,6 +14,26 @@ def main_menus_gui_import_func():
     global Config
     import Config
 
+    global MainGUI
+    import MainGUI
+
+
+    # Import locale and gettext modules for defining translation texts which will be recognized by gettext application (will be run by programmer externally) and exported into a ".pot" file. 
+    global _tr                                                                                # This arbitrary variable will be recognized by gettext application for extracting texts to be translated
+    import locale
+    from locale import gettext as _tr
+
+    # Define contstants for language translation support
+    global application_name
+    application_name = "system-monitoring-center"
+    translation_files_path = "/usr/share/locale"
+    system_current_language = os.environ.get("LANG")
+
+    # Define functions for language translation support
+    locale.bindtextdomain(application_name, translation_files_path)
+    locale.textdomain(application_name)
+    locale.setlocale(locale.LC_ALL, system_current_language)
+
 
 # ----------------------------------- MainMenusGUI - Main Menus GUI Function (the code of this module in order to avoid running them during module import and defines GUI functions/signals) -----------------------------------
 def main_menus_gui_func():
@@ -58,10 +78,10 @@ def main_menus_gui_func():
             global Thread
             from threading import Thread
         def main_menus_gui_open_terminal_func():
-            try:                                                                              # "try-catch" is used in order to avoid errors after terminal window is closed on Gnome desktop environment installed systems.
-                (subprocess.check_output(["x-terminal-emulator", "-e", "/bin/bash"], shell=False)).decode()
+            try:                                                                              # "try-catch" is used in order to avoid errors and show an error dialog to user if default terminal on the system can not be detected.
+                (subprocess.call(["x-terminal-emulator"], shell=False))
             except:
-                pass
+                main_menus_gui_system_default_terminal_detection_error_dialog()
         open_terminal_thread = Thread(target=main_menus_gui_open_terminal_func, daemon=True).start()    # Terminal is run in another thread in order not to wait end of the run which occurs in single threaded code execution.
 
     def on_checkmenuitem1001m_toggled(widget):                                                # "Floating Summary" menu item
@@ -120,3 +140,13 @@ def main_menus_gui_func():
     menuitem1004m.connect("activate", on_menuitem1004m_activate)
     menuitem1005m.connect("activate", on_menuitem1005m_activate)
     menuitem1006m.connect("activate", on_menuitem1006m_activate)
+
+
+# ----------------------------------- MainMenusGUI - Main Menu GUI System Default Terminal Detection Error Dialog Function (shows a warning dialog when a startup item is tried to be reset to system default which means user specific desktop file of the startup application will be deleted (system-wide values file will be untouched)) -----------------------------------
+def main_menus_gui_system_default_terminal_detection_error_dialog():
+
+    error_dialog1001 = Gtk.MessageDialog(transient_for=MainGUI.window1, title=_tr("Error"), flags=0, message_type=Gtk.MessageType.ERROR,
+    buttons=Gtk.ButtonsType.CLOSE, text=_tr("Default Terminal Application Not Detected"), )
+    error_dialog1001.format_secondary_text(_tr("Default terminal application on this system could not be detected."))
+    error_dialog1001.run()
+    error_dialog1001.destroy()
