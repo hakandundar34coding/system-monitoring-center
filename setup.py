0f8fa9ef7041ce48b61dc53045df4fef9e1d3d17
Hakan Dündar <hakandundar34coding@gmail.com>
2022-03-14 11:49:05 +0300

    Minor improvements

--- setup.py
+++ setup.py
@@ -23,9 +23,6 @@ def files_in_folder(folder):
 
 
 PREFIX = "/usr"
-if "--flatpak" in sys.argv:
-    PREFIX = "/app"
-    sys.argv.remove("--flatpak")
 
 if "--rpm" in sys.argv:
     PREFIX = "RPM"
@@ -34,41 +31,12 @@ if "--rpm" in sys.argv:
     del sys.argv[-1]
 
 
-if PREFIX == "/app":
-    os.rename("icons/apps/system-monitoring-center.svg", "icons/apps/com.github.hakand34.system-monitoring-center.svg")
-    icon_list = os.listdir("icons/actions/")
-    for icon in icon_list:
-        os.rename("icons/actions/" + icon, "icons/actions/com.github.hakand34.system-monitoring-center." + icon.split("system-monitoring-center-")[-1])
-
-    with open("integration/com.github.hakand34.system-monitoring-center.desktop") as reader:
-        desktop_file_content = reader.read()
-    desktop_file_content = desktop_file_content.replace("Icon=system-monitoring-center", "Icon=com.github.hakand34.system-monitoring-center")
-    with open("integration/com.github.hakand34.system-monitoring-center.desktop", "w") as writer:
-        writer.write(desktop_file_content)
-
-    with open("integration/system-monitoring-center") as reader:
-        script_file_content = reader.read()
-    script_file_content = script_file_content.replace("/usr", "/app")
-    with open("integration/system-monitoring-center", "w") as writer:
-        writer.write(script_file_content)
-
-    data_files = [
-        ("/app/share/applications/", ["integration/com.github.hakand34.system-monitoring-center.desktop"]),
-        ("/app/share/locale/tr/LC_MESSAGES/", ["translations/tr/system-monitoring-center.mo"]),
-        ("/app/share/locale/pt_BR/LC_MESSAGES/", ["translations/pt_BR/system-monitoring-center.mo"]),
-        ("/app/share/man/man1/", ["man/system-monitoring-center.1.gz"]),
-        ("/app/share/system-monitoring-center/src/", files_in_folder("src/")),
-        ("/app/share/system-monitoring-center/ui/", files_in_folder("ui/")),
-        ("/app/share/icons/hicolor/scalable/actions/", files_in_folder("icons/actions/")),
-        ("/app/share/icons/hicolor/scalable/apps/", ["icons/apps/com.github.hakand34.system-monitoring-center.svg"]),
-        ("/app/share/polkit-1/actions/", ["integration/com.github.hakand34.system-monitoring-center.policy"]),
-        ("/app/bin/", ["integration/system-monitoring-center"])
-    ]
-
 if PREFIX == "RPM":
     os.chmod("integration/com.github.hakand34.system-monitoring-center.desktop", 0o644)
-    os.chmod("translations/tr/system-monitoring-center.mo", 0o644)
+    os.chmod("translations/cs/system-monitoring-center.mo", 0o644)
+    os.chmod("translations/pl/system-monitoring-center.mo", 0o644)
     os.chmod("translations/pt_BR/system-monitoring-center.mo", 0o644)
+    os.chmod("translations/tr/system-monitoring-center.mo", 0o644)
     os.chmod("man/system-monitoring-center.1.gz", 0o644)
     for file in files_in_folder("src/"):
         os.chmod(file, 0o644)
@@ -81,8 +49,10 @@ if PREFIX == "RPM":
     os.chmod("integration/system-monitoring-center", 0o644)
     data_files = [
         (build_root + "/usr/share/applications/", ["integration/com.github.hakand34.system-monitoring-center.desktop"]),
-        (build_root + "/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/system-monitoring-center.mo"]),
+        (build_root + "/usr/share/locale/cs/LC_MESSAGES/", ["translations/cs/system-monitoring-center.mo"]),
+        (build_root + "/usr/share/locale/pl/LC_MESSAGES/", ["translations/pl/system-monitoring-center.mo"]),
         (build_root + "/usr/share/locale/pt_BR/LC_MESSAGES/", ["translations/pt_BR/system-monitoring-center.mo"]),
+        (build_root + "/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/system-monitoring-center.mo"]),
         (build_root + "/usr/share/man/man1/", ["man/system-monitoring-center.1.gz"]),
         (build_root + "/usr/share/system-monitoring-center/src/", files_in_folder("src/")),
         (build_root + "/usr/share/system-monitoring-center/ui/", files_in_folder("ui/")),
@@ -98,14 +68,18 @@ if PREFIX != "/app" and PREFIX != "RPM":
     for file in files_in_folder("icons/actions/"):
         os.chmod(file, 0o644)
     os.chmod("icons/apps/system-monitoring-center.svg", 0o644)
-    os.chmod("translations/tr/system-monitoring-center.mo", 0o644)
+    os.chmod("translations/cs/system-monitoring-center.mo", 0o644)
+    os.chmod("translations/pl/system-monitoring-center.mo", 0o644)
     os.chmod("translations/pt_BR/system-monitoring-center.mo", 0o644)
+    os.chmod("translations/tr/system-monitoring-center.mo", 0o644)
     os.chmod("integration/com.github.hakand34.system-monitoring-center.policy", 0o644)
     os.chmod("src/__version__", 0o644)
     data_files = [
         ("/usr/share/applications/", ["integration/com.github.hakand34.system-monitoring-center.desktop"]),
-        ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/system-monitoring-center.mo"]),
+        ("/usr/share/locale/cs/LC_MESSAGES/", ["translations/cs/system-monitoring-center.mo"]),
+        ("/usr/share/locale/pl/LC_MESSAGES/", ["translations/pl/system-monitoring-center.mo"]),
         ("/usr/share/locale/pt_BR/LC_MESSAGES/", ["translations/pt_BR/system-monitoring-center.mo"]),
+        ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/system-monitoring-center.mo"]),
         ("/usr/share/man/man1/", ["man/system-monitoring-center.1.gz"]),
         ("/usr/share/system-monitoring-center/src/", files_in_folder("src/")),
         ("/usr/share/system-monitoring-center/ui/", files_in_folder("ui/")),
