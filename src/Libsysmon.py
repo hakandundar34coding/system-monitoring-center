a16e788f60a872f263c07536e0febaea152b5344
Hakan Dündar <hakandundar34coding@gmail.com>
2026-02-15 09:39:08 +0300

    Minor code simplifications (Libsysmon)

--- src/Libsysmon.py
+++ src/Libsysmon.py
@@ -4151,12 +4151,11 @@ def get_username_uid_dict():
 
     environment_type = get_environment_type()
 
-    if environment_type == "flatpak":
-        with open("/var/run/host/etc/passwd") as reader:
-            etc_passwd_lines = reader.read().strip().split("\n")
-    else:
-        with open("/etc/passwd") as reader:
-            etc_passwd_lines = reader.read().strip().split("\n")
+    command_list = ["cat", "/etc/passwd"]
+    if get_environment_type() == "flatpak":
+        command_list = ["flatpak-spawn", "--host"] + command_list
+    etc_passwd_output = (subprocess.check_output(command_list, shell=False)).decode().strip()
+    etc_passwd_lines = etc_passwd_output.split("\n")
 
     username_uid_dict = {}
     for line in etc_passwd_lines:
@@ -5078,9 +5077,7 @@ def get_os_name_version_codename_based_on():
     command_list = ["cat", "/etc/os-release"]
     if get_environment_type() == "flatpak":
         command_list = ["flatpak-spawn", "--host"] + command_list
-
     os_release_output = (subprocess.check_output(command_list, shell=False)).decode().strip()
-
     os_release_output_lines = os_release_output.strip().split("\n")
 
     os_name = "-"
